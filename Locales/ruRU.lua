be7deba9fa251930ea17782abdba363f89a750fa
dak1ne-4th <57269677+dak1ne-4th@users.noreply.github.com>
2019-11-01 22:19:09 +0300

    Update ruRU locale
    Lets try this and collect feedback. 
    or... i`ll collect feed back from myself and mates

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -9,23 +9,23 @@ L.flagRespawns = "Появление Флагов"
 
 L.takenTheFlagTrigger = "^(.+) захватывает флаг!"
 L.hasTakenTheTrigger = "захватил"
---L.upgradeToTrigger = "Upgrade to"
+L.upgradeToTrigger = "Улучшено" -- тут еще вопрос (!) проверить на проде // spellcheck on live
 L.droppedTrigger = "уронил"
 L.capturedTheTrigger = "захватил"
 
---L.hordeGate = "Horde Gate"
---L.allianceGate = "Alliance Gate"
---L.gatePosition = "%s (%s)"
---L.west = "West"
---L.front = "Front"
---L.east = "East"
---L.hordeBoss = "Horde Boss"
---L.allianceBoss = "Alliance Boss"
---L.galvangar = "Galvangar"
---L.balinda = "Balinda"
+L.hordeGate = "Ворота Орды"
+L.allianceGate = "Ворота Альянса"
+L.gatePosition = "%s (%s)" -- тут точно нужен перевод?// check on live
+L.west = "Запад"
+L.front = "Передовая" -- тут еще вопрос (!) проверить на проде // spellcheck on live
+L.east = "Восток"
+L.hordeBoss = "Босс Орды"
+L.allianceBoss = "Босс Альянса"
+L.galvangar = "Гальвангар" -- Капитан Гальвангар <Капитан клана Северного Волка>
+L.balinda = "Балинда" -- Капитан Балинда Каменный Очаг <Капитан клана Грозовой Вершины>
 L.ivus = "Ивус" -- Ивус Лесной Властелин
 L.lokholar = "Локолар" -- Локолар Владыка Льда
---L.handIn = "|cFF33FF99Capping|r: Automatically handing in quest items."
+L.handIn = "|cFF33FF99Capping|r: Автоматическая сдачать квестовых предметов." -- проверить на проде // spellcheck on live
 
 --- Alliance IoC Workshop yells:
 -- Gnomish Mechanic yells: I'm halfway there! Keep the Horde away from here.  They don't teach fighting in engineering school!
@@ -33,26 +33,26 @@ L.lokholar = "Локолар" -- Локолар Владыка Льда
 --- Horde IoC Workshop yells:
 -- Goblin Mechanic yells: I'm about halfway done! Keep the Alliance away - fighting's not in my contract!
 -- Goblin Mechanic yells: It's broken again?! I'll fix it... just don't expect the warranty to cover this.
---L.halfway = "halfway"
---L.broken = "broken"
+L.halfway = "почти" -- на самом деле должно быть "наполовину", если достловно то полпути, но надо смотреть на проде // spellcheck on live
+L.broken = "сломано" -- надо смотреть на проде // spellcheck on live
 
 -- Wintergrasp
---L.damaged = "|cFF33FF99Capping|r: %s Damaged"
---L.destroyed = "|cFF33FF99Capping|r: %s Destroyed"
---L.northEastKeep = "North-East Fortress Tower"
---L.southEastKeep = "South-East Fortress Tower"
---L.northWestKeep = "North-West Fortress Tower"
---L.southWestKeep = "South-West Fortress Tower"
---L.northWest = "North-West Wall"
---L.southWest = "South-West Wall"
---L.south = "South Wall"
---L.southEast = "South-East Wall"
---L.northEast = "North-East Wall"
---L.innerWest = "Inner-West Wall"
---L.innerSouth = "Inner-South Wall"
---L.innerEast = "Inner-East Wall"
---L.southGate = "South Gate"
---L.mainEntrance = "Main Entrance"
---L.westTower = "West Tower"
---L.southTower = "South Tower"
---L.eastTower = "East Tower"
+L.damaged = "|cFF33FF99Capping|r: %s подвергается нападению" -- проверить на проде, подумать над универсальным вариантом // spellcheck on live
+L.destroyed = "|cFF33FF99Capping|r: %s разрушена" -- проверить на проде, подумать над универсальным вариантом // spellcheck on live
+L.northEastKeep = "Северо-восточная башня крепости"
+L.southEastKeep = "Юго-восточная башня крепости"
+L.northWestKeep = "Северо-западная башня крепости"
+L.southWestKeep = "Юго-западная башня крепости"
+L.northWest = "Северо-западная стена"
+L.southWest = "Юго-западная стена"
+L.south = "Юная стена"
+L.southEast = "Юго-восточная стена"
+L.northEast = "Северо-восточная стена"
+L.innerWest = "Внутренняя западная стена"
+L.innerSouth = "Внутренняя южная стена"
+L.innerEast = "Внутренняя востовная стена"
+L.southGate = "Южные ворота"
+L.mainEntrance = "Главный вход"
+L.westTower = "Западная башня"
+L.southTower = "Южная башня"
+L.eastTower = "Восточная башня"
